bc473171d4b894c42a62db0d6a776c7673f66ad1
Peyman-Sabzizade <peyman.front.developer@gmail.com>
2025-01-02 00:52:42 +0330

    Config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+  "./src/**/*.{html,js}",
+  "./*.{html,js}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
