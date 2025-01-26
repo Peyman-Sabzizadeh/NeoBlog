b775b0e016208e4ada1eed0f87cff1b5efe1295f
Peyman-Sabzizade <peyman.front.developer@gmail.com>
2025-01-27 01:16:35 +0330

    Add captcha

--- src/script/login.js
+++ src/script/login.js
@@ -3,8 +3,35 @@ const nameInput = $.querySelector("#name-input")
 const phoneInput = $.querySelector("#phone-input")
 const usernameInput = $.querySelector("#username-input")
 const passwordInput = $.querySelector("#password-input")
+const cpatchaInput = $.querySelector("#captcha-input")
+const captchaImg = $.querySelector("#captcha-img")
 const loginBtn = $.querySelector("#login-btn")
 const signupBtn = $.querySelector("#signup-btn")
+function captchaGenerator () {
+  const captchaQuery = `
+  query {
+    generateCaptcha {
+      captcha
+      uuid
+    }
+  }
+  `;
+  fetch("http://localhost:5005/blog", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: captchaQuery
+    }),
+  })
+  .then((response) => response.json())
+  .then((info) => 
+    captchaImg.innerHTML = info.data.generateCaptcha.captcha
+  )
+  .catch((error) => console.error("Error:", error));
+}
+window.addEventListener("load",captchaGenerator)
 function register (event) {
     event.preventDefault()
     const mutation = `
