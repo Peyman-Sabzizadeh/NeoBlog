15fecfac0bf7d6fac76b911c8e56286c3a2f0eb8
Peyman-Sabzizade <peyman.front.developer@gmail.com>
2025-01-30 14:25:44 +0330

    Add autoLogin future

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let $ = document
 const lightIcon = $.querySelector("#light-icon")
 const darkIcon = $.querySelector("#dark-icon")
+const loginBtn = $.querySelector(".login-btn")
+const signupBtn = $.querySelector(".signup-btn")
 let classTheme = document.documentElement
 let getTheme = localStorage.getItem("theme")
 function setTheme () {
@@ -29,3 +31,25 @@ function changeToLight () {
 }
 lightIcon.addEventListener("click",changeToDark)
 darkIcon.addEventListener("click",changeToLight)
+function autoLogin () {
+    let getAccessToken = localStorage.getItem("token")
+    const getMeQuery = `
+    query GetMe {
+        getMe {
+        name  
+        }
+    }`;
+    fetch("http://localhost:5005/blog", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `${getAccessToken}`
+        },
+        body: JSON.stringify({
+            query: getMeQuery,
+        }),
+    })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+}
+window.addEventListener("load",autoLogin)
